48473ee98a75a83328b452a1eec014e6c364b203
ben9583 <bplate9583@gmail.com>
2020-07-25 22:14:36 -0700

    Bug fixes and tweaks

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glob
 import tensorflow as tf
 import numpy as np
 import os
+import sys
 import wandb
 from wandb.keras import WandbCallback
 
@@ -22,7 +23,7 @@ DATA_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), "data", "im
 BATCH_SIZE = 8
 EPOCHS = 10
 
-image_count = len(glob.glob(target_dir + os.path.sep + "inputs"))
+image_count = len(glob.glob(DATA_DIR + os.path.sep + "inputs"))
 image_width = 512
 image_height = 512
 
@@ -39,7 +40,7 @@ def decode_bw_img(img):
 
 def process_path(file_path):
 	input_image = tf.io.read_file(file_path)
-	output_image = tf.io.read_file(os.path.join(DATA_DIR, "outputs", os.path.basename(file_path)))
+	output_image = tf.io.read_file(os.path.join(DATA_DIR, "outputs", os.path.basename(file_path.numpy().decode("utf-8"))))
 
 	input_image = decode_colored_img(input_image)
 	output_image = decode_bw_img(output_image)
@@ -49,7 +50,7 @@ def process_path(file_path):
 def configure_for_performance(ds):
 	ds = ds.cache()
 	ds = ds.shuffle(buffer_size=1000)
-	ds = ds.batch(batch_size)
+	ds = ds.batch(BATCH_SIZE)
 	ds = ds.prefetch(buffer_size=AUTOTUNE)
 	return ds
 
@@ -58,24 +59,25 @@ print("Creating model...")
 model = tf.keras.Sequential()
 model.add(tf.keras.layers.Conv2D(8, (3, 3), activation='relu', input_shape=input_shape))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu')
+model.add(tf.keras.layers.Conv2D(16, (3, 3), activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(tf.keras.layers.MaxPooling2D((2, 2)))
-model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu')
+model.add(tf.keras.layers.Conv2D(32, (3, 3), activation='relu'))
 model.add(tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dense(1024), activation='relu')
-model.add(tf.keras.layers.Dense(1024), activation='relu')
-model.add(tf.keras.layers.Conv2DTranspose(32, (3, 3), strides=2, padding="same" activation='relu')
-model.add(tf.keras.layers.Conv2DTranspose(32, (3, 3), strides=2, padding="same" activation='relu')
-model.add(tf.keras.layers.Conv2DTranspose(16, (3, 3), strides=2, padding="same" activation='relu')
-model.add(tf.keras.layers.Conv2DTranspose(8, (3, 3), strides=2, padding="same" activation='relu')
-model.add(tf.keras.layers.Conv2DTranspose(1, (3, 3), strides=2, padding="same" activation='relu')
+model.add(tf.keras.layers.Dense(1024, activation='relu'))
+model.add(tf.keras.layers.Dense(1024, activation='relu'))
+model.add(tf.keras.layers.Reshape((16, 16, 4)))
+model.add(tf.keras.layers.Conv2DTranspose(32, (3, 3), strides=2, padding="same", activation='relu'))
+model.add(tf.keras.layers.Conv2DTranspose(32, (3, 3), strides=2, padding="same", activation='relu'))
+model.add(tf.keras.layers.Conv2DTranspose(16, (3, 3), strides=2, padding="same", activation='relu'))
+model.add(tf.keras.layers.Conv2DTranspose(8, (3, 3), strides=2, padding="same", activation='relu'))
+model.add(tf.keras.layers.Conv2DTranspose(1, (3, 3), strides=2, padding="same", activation='relu'))
 
-model.compile(loss=tf.keras.losses.MSE(), optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
 model.summary()
+model.compile(loss='MSE', optimizer=tf.keras.optimizers.Adam(learning_rate=0.0001), metrics=[tf.keras.metrics.BinaryAccuracy(), tf.keras.metrics.Precision(), tf.keras.metrics.Recall()])
 
 checkpoint_path = "/content/training_2/cp-{epoch:04d}.ckpt"
 checkpoint_dir = os.path.dirname(checkpoint_path)
@@ -90,17 +92,19 @@ model.save_weights(checkpoint_path.format(epoch=0))
 
 print("Compiling data...")
 
-
-train_ds = tf.data.Dataset.list_files(str(DATA_DIR/'inputs/*'), shuffle=False)
-train_ds = train_ds.shuffle(image_count, reshuffle_each_iteration=False)
-train_ds = train_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+train_ds = tf.data.Dataset.list_files(str(DATA_DIR + '/inputs/*.png'))
+train_ds = train_ds.map(lambda x: tf.py_function(process_path, [x], [tf.string]), num_parallel_calls=AUTOTUNE)
 train_ds = configure_for_performance(train_ds)
 
-test_ds = tf.data.Dataset.list_files(str(DATA_DIR/'validation_inputs/*'), shuffle=False)
-test_ds = test_ds.shuffle(image_count, reshuffle_each_iteration=False)
-test_ds = test_ds.map(process_path, num_parallel_calls=AUTOTUNE)
+test_ds = tf.data.Dataset.list_files(str(DATA_DIR + '/validation_inputs/*.png'))
+test_ds = test_ds.map(lambda x: tf.py_function(process_path, [x], [tf.string]), num_parallel_calls=AUTOTUNE)
 test_ds = configure_for_performance(test_ds)
 
-model.fit(x=train_ds, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=2, callbacks=[cp_callback, WandbCallback()], validation_data=test_ds)
+print(type(train_ds))
+print(train_ds)
+print(type(train_ds.as_numpy_iterator()))
+print(train_ds.as_numpy_iterator())
 
+list(train_ds.as_numpy_iterator())
 
+#model.fit(x=train_ds, batch_size=BATCH_SIZE, epochs=EPOCHS, verbose=2, callbacks=[cp_callback, WandbCallback()], validation_data=test_ds)
